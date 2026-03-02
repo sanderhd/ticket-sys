b6d3de1380188845a71020c16b3d041024617289
Sander <fristislekker@proton.me>
2026-03-02 15:49:20 +0100

    feat: add TicketFactory for generating ticket data and update DatabaseSeeder for consistency

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,14 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        User::factory(10)->create(); 
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
 
-
-        Ticket::factory(20)->create();
+        Ticket::factory(20)->create(); 
     }
 }
